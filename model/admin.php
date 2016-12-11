c68dceff36e6c484dc0d903ff4a841a11c7d9707
TJ LaBaugh <tjlabaugh@gmail.com>
2016-12-10 23:39:16 -0500

    began to add login features

--- model/admin.php
+++ model/admin.php
@@ -1,13 +1,27 @@
 <?php
-	function add_admin($email, $password) {
-	    global $db;
-	    $password = sha1($email . $password);
-	    $query = 'INSERT INTO administrators (emailAddress, password)
-	              VALUES (:email, :password)';
+	echo "Hello";
+
+	$givenusername = filter_input(INPUT_POST, 'username');
+	$givenpassword = filter_input(INPUT_POST, 'password');
+
+	echo " " . $givenusername;
+	echo " " . $givenpassword . " ";
+
+	function isValidLogin($givenusername, $givenpassword) {
+		include('database.php');
+	//  $password = sha1($email . $password);
+	    $query = 'SELECT admin_id FROM admin
+	              WHERE username = :username AND password = :password';
 	    $statement = $db->prepare($query);
-	    $statement->bindValue(':email', $email);
-	    $statement->bindValue(':password', $password);
+	    $statement->bindValue(':username', $givenusername, PDO::PARAM_STR);
+	    $statement->bindValue(':password', $givenpassword, PDO::PARAM_STR);
 	    $statement->execute();
+	    $valid = ($statement->rowCount() == 1);
 	    $statement->closeCursor();
+	    return $valid;
 	}
+
+	$valid = isValidLogin($givenusername, $givenpassword);
+	echo " // " . $valid;
+
 ?>
